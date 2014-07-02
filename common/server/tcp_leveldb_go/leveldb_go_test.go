3afdca0b90c85b700e4ac51691c7f78e413e55ff
goodplayer <woshiyigelaji@gmail.com>
2014-07-02 23:35:14 +0800

    add use case

--- common/server/tcp_leveldb_go/leveldb_go_test.go
+++ common/server/tcp_leveldb_go/leveldb_go_test.go
@@ -4,6 +4,7 @@ import (
 	"code.google.com/p/leveldb-go/leveldb"
 	"os"
 	// "code.google.com/p/leveldb-go/leveldb/db"
+	"fmt"
 	"testing"
 )
 
@@ -26,5 +27,21 @@ func Test_leveldb_go_basic_use(t *testing.T) {
 	}
 	if string(data) != "bb" {
 		t.Error("not equal")
+	}
+}
+
+func Test_leveldb_go_use_2(t *testing.T) {
+	defer os.RemoveAll("./temp.db")
+	openDb, err := leveldb.Open("./temp.db", nil)
+	if err != nil {
+		t.Error("error occurs", err)
+		return
+	}
+	openDb.Set([]byte("key1"), []byte("value1"), nil)
+	openDb.Set([]byte("bbbb"), []byte("bbbbvalue"), nil)
+	openDb.Set([]byte("mul"), []byte("mulvalue"), nil)
+	iter := openDb.Find([]byte("key1"), nil)
+	if iter.Next() {
+		fmt.Println(iter.Key())
 	}
 }
